6412eafdfc726d9d1fa7fb72e21e1c3bcb29f002
Thomas Tuegel <ttuegel@gmail.com>
2015-01-25 15:43:43 -0600

    default cursor color white

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,6 +31,7 @@
    (quote
     (".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".hi" ".elc")))
  '(custom-safe-themes t)
+ '(default-frame-alist (quote ((cursor-color . "white"))))
  '(el-get-user-package-directory "~/.emacs.d/el-get-user/init")
  '(evil-shift-width 2)
  '(font-latex-math-environments
