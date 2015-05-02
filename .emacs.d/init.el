376392257989d25b0cf2c2f110e41b6b0fbee09a
Thomas Tuegel <ttuegel@gmail.com>
2015-05-02 07:44:50 -0500

    un-customize uniquify

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -55,13 +55,12 @@
  '(completion-ignored-extensions
    (quote
     (".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".hi" ".elc")))
- '(custom-safe-themes t)
  '(evil-shift-width 2)
+ '(font-latex-fontify-script nil)
+ '(font-latex-fontify-sectioning (quote color))
  '(font-latex-math-environments
    (quote
     ("display" "displaymath" "equation" "eqnarray" "gather" "multline" "align" "alignat" "xalignat" "dmath" "math")))
- '(font-latex-fontify-sectioning 'color)
- '(font-latex-fontify-script nil)
  '(haskell-indentation-cycle-warn nil)
  '(haskell-indentation-starter-offset 2)
  '(haskell-literate-default (quote tex))
@@ -108,7 +107,6 @@
  '(tab-always-indent t)
  '(tab-stop-list (number-sequence 2 120 2))
  '(tab-width 2)
- '(uniquify-buffer-name-style (quote forward) nil (uniquify))
  '(whitespace-style (quote (face trailing tabs))))
 
 (custom-set-faces
@@ -131,6 +129,12 @@
 (diminish 'global-whitespace-mode)
 (show-paren-mode t)
 (electric-indent-mode t)
+
+;; Make buffer names more unique
+(use-package uniquify
+  :demand t
+  :config
+  (setq uniquify-buffer-name-style 'forward))
 
 (use-package evil-leader
   :commands (global-evil-leader-mode)
@@ -166,9 +170,6 @@
   (evil-define-command evil-shift-left-line (count)
     (interactive "<c>")
     (ttuegel/evil-shift-line count 1)))
-
-(use-package uniquify
-  :demand t)
 
 (defun evil-map (key def &rest bindings)
   (evil-leader--def-keys evil-normal-state-map key def bindings)
