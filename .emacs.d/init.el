5e5d001abf525e3853bba8f5f4f3bb13859462df
Thomas Tuegel <ttuegel@gmail.com>
2015-08-17 07:36:20 -0500

    font-lock-type-face: disable italics

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,7 +58,10 @@
    (quote
     (".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".hi" ".elc"))))
 
-(custom-theme-set-faces 'user '(default ((t (:family "Source Code Pro")))))
+(custom-theme-set-faces
+ 'user
+ '(default ((t (:family "Source Code Pro"))))
+ '(font-lock-type-face ((t :slant normal))))
 
 ;; tab stop settings
 (custom-theme-set-variables
