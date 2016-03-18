145a3bce312f1c9758ac987b197e0e40c287e9e4
Thomas Tuegel <ttuegel@gmail.com>
2016-03-18 07:51:31 -0500

    separate haskell-mode configuration

--- init.el
+++ init.el
@@ -430,34 +430,7 @@ used to fill a paragraph to `ttuegel/LaTeX-auto-fill-function'."
 (require 'flycheck)
 (setq flycheck-checkers (delq 'haskell-hlint flycheck-checkers))
 
-;; ghc-mod
-
-(require 'ghc)
-(custom-theme-set-variables 'user '(ghc-sort-key nil))
-
-;; haskell-mode
-
-(require 'haskell-mode)
-
-(custom-theme-set-variables
-  'user
-  '(haskell-literate-default 'tex)
-  '(haskell-process-auto-import-loaded-modules nil)
-  '(haskell-process-log t)
-  '(haskell-process-suggest-remove-import-lines nil)
-  '(haskell-process-type 'cabal-repl)
-  '(haskell-indentation-ifte-offset 4)
-  '(haskell-indentation-layout-offset 4)
-  '(haskell-indentation-left-offset 4))
-
-(add-hook 'haskell-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'haskell-mode-hook #'yas-minor-mode)
-(add-hook 'haskell-mode-hook #'turn-on-haskell-indentation)
-
-(require 'hindent)
-(custom-theme-set-variables 'user '(hindent-style "johan-tibell"))
-(add-hook 'haskell-mode-hook #'hindent-mode)
-(diminish 'hindent-mode)
+(require 'ttuegel-haskell-config)
 
 ;; nix-mode
 
---
+++ lisp/ttuegel-haskell-config.el
@@ -0,0 +1,33 @@
+;;; ttuegel-haskell-config.el -- ttuegel's Haskell configuration
+;;; Commentary:
+;;; Code:
+
+; Disable the HLint flycheck checker.
+(require 'flycheck)
+(setq flycheck-checkers (delq 'haskell-hlint flycheck-checkers))
+
+; Enable ghc-mod.
+(require 'ghc)
+(custom-theme-set-variables 'user '(ghc-sort-key nil))
+
+(require 'haskell-mode)
+
+(custom-theme-set-variables
+  'user
+  '(haskell-literate-default 'tex)
+  '(haskell-process-auto-import-loaded-modules nil)
+  '(haskell-process-log t)
+  '(haskell-process-suggest-remove-import-lines nil)
+  '(haskell-process-type 'cabal-repl))
+
+(add-hook 'haskell-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'haskell-mode-hook #'yas-minor-mode)
+(add-hook 'haskell-mode-hook #'turn-on-haskell-indent)
+
+(require 'hindent)
+(custom-theme-set-variables 'user '(hindent-style "johan-tibell"))
+(add-hook 'haskell-mode-hook #'hindent-mode)
+(diminish 'hindent-mode)
+
+(provide 'ttuegel-haskell-config)
+;;; ttuegel-haskell-config.el ends here
