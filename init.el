774c17918ac17f67360f3d825eb0045c9952f9c8
Thomas Tuegel <ttuegel@gmail.com>
2016-09-26 10:41:04 -0500

    init: set selected packages

--- init.el
+++ init.el
@@ -16,7 +16,24 @@
 (eval-when-compile
   (require 'use-package))
 
-(setq use-package-always-ensure t)
+(customize-set-variable
+ 'package-selected-packages
+ '(intero
+   yaml-mode
+   ledger-mode
+   nix-mode
+   markdown-mode
+   auctex
+   git-timemachine
+   magit
+   avy
+   helm
+   evil-indent-textobject
+   evil-surround
+   rainbow-delimiters
+   monokai-theme
+   bind-key
+   diminish))
 
 (use-package diminish :demand)
 (use-package bind-key :demand)
@@ -452,27 +469,3 @@ only whitespace."
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auto-save-default nil)
- '(blink-cursor-mode nil)
- '(default-frame-alist (quote ((cursor-color . "white"))))
- '(inhibit-startup-screen t)
- '(make-backup-files nil)
- '(package-selected-packages
-   (quote
-    (intero yaml-mode ledger-mode nix-mode markdown-mode auctex git-timemachine magit avy helm evil-indent-textobject evil-surround rainbow-delimiters monokai-theme bind-key diminish)))
- '(select-enable-clipboard nil)
- '(uniquify-buffer-name-style (quote forward) nil (uniquify))
- '(visible-bell t)
- '(x-select-enable-clipboard nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro"))))
- '(font-lock-type-face ((t :slant normal))))
