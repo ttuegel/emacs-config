a45e02a08695ba87f03d6919cfbc80a61e176be8
Thomas Tuegel <ttuegel@gmail.com>
2016-10-20 10:58:58 -0500

    externalize bibtex-fetch

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
 (package-initialize)
 
 (add-to-list 'load-path "~/el/biblio.el")
+(add-to-list 'load-path "~/el/bibtex-fetch")
 (add-to-list 'load-path "~/.emacs.d/use-package")
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
@@ -403,6 +404,7 @@ only whitespace."
 (use-package helm-bibtex)
 (require 'biblio)
 (require 'bibtex-normalize)
+(require 'bibtex-fetch)
 
 (setq bibtex-entry-format '(opts-or-alts numerical-fields inherit-booktitle last-comma delimiters unify-case braces strings sort-fields))
 (setq bibtex-text-indentation 0)
