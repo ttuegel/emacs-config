ae40ea4518ad8fdd1fbf56da55da164fef8a0702
Thomas Tuegel <ttuegel@gmail.com>
2017-02-06 13:36:18 -0600

    Set a larger font by default

--- init.el
+++ init.el
@@ -86,9 +86,8 @@
 
 ;; Use Source Code Pro font by default.
 
-(custom-theme-set-faces
- 'user
- '(default ((t (:family "Source Code Pro" :height 100)))))
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-12"))
+(set-face-attribute 'default t :font "Source Code Pro-12")
 
 ;; Don't use italics to indicate types.
 (custom-theme-set-faces 'user '(font-lock-type-face ((t :slant normal))))
