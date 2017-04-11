90589049a38ab235086ceae76913ac874a4228a8
Thomas Tuegel <ttuegel@mailbox.org>
2017-04-11 11:28:14 -0500

    org-bibtex-open-document: Run `bibtex-open-document' on Babel src blocks

--- init.el
+++ init.el
@@ -469,7 +469,16 @@ only whitespace."
   (electric-indent-local-mode -1))
 
 ;;; Org
+
+(defun org-bibtex-open-document ()
+  "Run `bibtex-open-document' if point is in a BibTeX source block."
+  (interactive)
+  (pcase (org-babel-get-src-block-info)
+    (`("bibtex" . ,_) (bibtex-open-document))
+    (_ (error "No BibTeX block at point"))))
+
 (use-package org
+  :bind (("C-c o" . org-bibtex-open-document))
   :config
   (setq org-catch-invisible-edits 'show)
   (add-hook 'org-mode-hook #'turn-off-electric-indent-local-mode)
