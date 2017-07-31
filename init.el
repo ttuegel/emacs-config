a0b1522e29d2c6533d26ff578eebffd32f4bcf60
Thomas Tuegel <ttuegel@mailbox.org>
2017-07-31 09:12:34 -0500

    Set default font to Iosevka Type

--- init.el
+++ init.el
@@ -90,8 +90,10 @@
 
 ;; Use Source Code Pro font by default.
 
-(add-to-list 'default-frame-alist '(font . "Hack-11"))
-(set-face-attribute 'default t :font "Hack-11")
+(set-face-attribute 'default t
+                    :family "Iosevka Type"
+                    :height 120
+                    :weight 'normal)
 
 ;; Don't use italics to indicate types.
 (custom-theme-set-faces 'user '(font-lock-type-face ((t :slant normal))))
