e58e8cb4bd3d8a2263a09e48cdb0f2b3ef61e84e
Thomas Tuegel <ttuegel@mailbox.org>
2017-12-24 06:06:32 -0600

    helm: Don't use global override for M-x

--- init.el
+++ init.el
@@ -173,7 +173,7 @@ only whitespace."
       "rg --color=always --smart-case --no-heading --line-number %s %s %s")
 
 ;; Rebind `M-x'
-(bind-key* "M-<SPC>" 'helm-M-x)
+(bind-key "M-<SPC>" 'helm-M-x)
 
 (let ((map ctl-x-map))
   (unbind-key "C-f" map)
