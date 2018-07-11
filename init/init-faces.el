d6c9c5e640b22e9c248e70c18273eb15805e28ea
Thomas Tuegel <ttuegel@mailbox.org>
2018-07-11 09:34:02 -0500

    init-faces: Use Fontconfig name for consistency
    Using Fontconfig names is required to select unusual weights like "Regular".

--- init/init-faces.el
+++ init/init-faces.el
@@ -3,8 +3,8 @@
 
 ;; Use Iosevka font by default.
 
-(set-frame-font (font-spec :family "Iosevka Type" :size 12.0 :weight 'normal))
-(set-face-attribute 'default nil :family "Iosevka Type" :height 120 :weight 'normal)
+(set-frame-font (font-spec :font "Iosevka Type Regular-12"))
+(set-face-attribute 'default nil :font "Iosevka Type Regular-12")
 
 
 ;; Don't use italics to indicate types.
