b15173da559c692b1c7d2c9bef1badfbedd764a7
Thomas Tuegel <ttuegel@mailbox.org>
2020-01-01 06:26:43 -0600

    Get solarized-emacs from Melpa

--- init.el
+++ init.el
@@ -340,20 +340,25 @@ only whitespace."
 
 ;; Colors
 
-(setq solarized-use-variable-pitch nil)
-(setq solarized-high-contrast-mode-line t)
-(setq solarized-height-minus-1 1.0)
-(setq solarized-height-plus-1 1.0)
-(setq solarized-height-plus-2 1.0)
-(setq solarized-height-plus-3 1.0)
-(setq solarized-height-plus-4 1.0)
 (setq custom-safe-themes t)
 
 (use-package solarized-theme
   :demand
-  :load-path "./solarized-emacs"
   :config
-  (load-theme 'solarized-light))
+  (setq solarized-use-variable-pitch nil)
+  (setq solarized-high-contrast-mode-line t)
+  (setq solarized-height-minus-1 1.0)
+  (setq solarized-height-plus-1 1.0)
+  (setq solarized-height-plus-2 1.0)
+  (setq solarized-height-plus-3 1.0)
+  (setq solarized-height-plus-4 1.0)
+  (load-theme 'solarized-light)
+  )
+
+;; Draw a dark box around the mode-line, even on inactive windows.
+(custom-theme-set-faces 'user
+  '(mode-line-inactive ((t . (:box t :overline nil :underline nil))) t)
+  )
 
 (add-to-list 'load-path (relative "./config"))
 
