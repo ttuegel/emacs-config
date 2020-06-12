6fb66782d0275fd6c33cb09e349513e7e301742c
Thomas Tuegel <ttuegel@mailbox.org>
2020-06-12 08:40:09 -0500

    flymake: Do not save temporary files in place

--- init.el
+++ init.el
@@ -499,6 +499,15 @@ only whitespace."
 (add-hook 'emacs-lisp-mode-hook #'flycheck-mode)
 
 
+;;; flymake
+
+(use-package flymake
+  :defines flymake-run-in-place
+  :config
+  (setq flymake-run-in-place nil)
+  )
+
+
 ;;; yasnippet
 
 (use-package yasnippet
