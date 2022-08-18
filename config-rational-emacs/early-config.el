64ef356c6fb10bba5c36900a37c558f142fc686b
Thomas Tuegel <ttuegel@mailbox.org>
2022-08-18 06:13:08 -0500

    Revert "Configure Rational Emacs"
    This reverts commit 5b74e6ad595f230e421964dfdbea19a34122b91a.

--- config-rational-emacs/early-config.el
+++
@@ -1,4 +0,0 @@
-;; Don't initialize `package.el' at startup so that Rational Emacs will skip
-;; updating the package archives. Updating the archives takes several seconds,
-;; but it isn't necessary because we get all our dependencies from Nix.
-(setq package-enable-at-startup nil)
