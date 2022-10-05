0e7210bc6c6f7e9b678c296aadc0f9f823cbcac5
Thomas Tuegel <thomas@mercury.com>
2022-10-05 13:12:51 -0500

    init.el: lsp: Disable file watchers

--- init.el
+++ init.el
@@ -436,6 +436,7 @@
   :custom (lsp-progress-via-spinner nil "Do not clutter the modeline with the slow spinner.")
   :custom (lsp-lens-enable nil "Do not clutter the buffer with code lens.")
   :custom (lsp-progress-function 'ignore)
+  :custom (lsp-enable-file-watchers nil)
   )
 
 (use-package lsp-ui
