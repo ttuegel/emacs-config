0e37eb7870f7a17bff9a07700abddc1074bfd50d
Thomas Tuegel <thomas@mercury.com>
2023-10-18 11:10:20 -0500

    winum: Bind keys 1..9 in window-map

--- init.el
+++ init.el
@@ -872,6 +872,17 @@ This is useful, e.g., for use with function `visual-line-mode'."
   :init
   ;; Prevent `winum-mode' from overriding my key choices.
   (setq winum-keymap (make-sparse-keymap))
+  :bind (:map window-map
+              ("1" . winum-select-window-1)
+              ("2" . winum-select-window-2)
+              ("3" . winum-select-window-3)
+              ("4" . winum-select-window-4)
+              ("5" . winum-select-window-5)
+              ("6" . winum-select-window-6)
+              ("7" . winum-select-window-7)
+              ("8" . winum-select-window-8)
+              ("9" . winum-select-window-9)
+              )
   )
 
 ;;; Garbage collection
